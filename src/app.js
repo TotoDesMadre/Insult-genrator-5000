be698ccc8bd37d97c2f0c0a5e94a9b8fd28a8eb5
TotoDesMadre <jinqiaoxincun@outlook.com>
2021-11-02 22:44:20 +0000

    Completed assignment

--- src/app.js
+++ src/app.js
@@ -5,7 +5,29 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = () => {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let randomNumber = Math.random() * 10;
+  document.querySelector("#the-insult").innerHTML = generateInsult();
+  console.log("" * +randomNumber);
+};
+
+let generateInsult = () => {
+  let subject = ["Yo Mama ", "Yo Auntie ", "Yo grandma "];
+  let insult = ["is so fat ", "is so ugly "];
+  let reaction = [
+    "when she fell I didnt laugh but the sidewalk cracked up.",
+    "when she skips a meal, the stock market drops.",
+    "it took me two buses and a train to get to her good side.",
+    "when she goes camping, the bears hide their food.",
+    "I swerved to miss her in my car and ran out of gas.",
+    "she was overthrown by a small militia group, and now she is known as the Republic of Yo Mama."
+  ];
+
+  let subIndx = Math.floor(Math.random() * subject.length);
+  let insultIndx = Math.floor(Math.random() * insult.length);
+  let reactionIndx = Math.floor(Math.random() * reaction.length);
+  return (
+    subject[subIndx] + "" + insult[insultIndx] + "" + reaction[reactionIndx]
+  );
 };
